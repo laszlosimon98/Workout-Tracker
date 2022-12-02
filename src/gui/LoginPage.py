621f272b176f1fc7294fbf4d604a3e732b52e6b0
Laszlo Simon <98slaci@gmail.com>
2022-12-02 10:49:15 +0100

    Login page centered

--- src/gui/LoginPage.py
+++ src/gui/LoginPage.py
@@ -14,23 +14,25 @@ class LoginPage(MainGUI):
         self.right_frame.destroy()
 
         self.label = customtkinter.CTkLabel(master=self.middle_frame, text="Login")
-        self.label.grid(row=1, column=0, pady=15, padx=10, sticky="ns")
+        self.label.grid(row=1, column=0, pady=15, padx=10, sticky="n")
 
-        self.username_entry = customtkinter.CTkEntry(master=self.middle_frame, placeholder_text="Username")
-        self.username_entry.grid(row=2, column=0, pady=(0, 15), padx=10, sticky="ns")
+        self.username_entry = customtkinter.CTkEntry(master=self.middle_frame, placeholder_text="Username", width=200)
+        self.username_entry.grid(row=2, column=0, pady=(0, 15), padx=10, sticky="n")
 
-        self.password_entry = customtkinter.CTkEntry(master=self.middle_frame, placeholder_text="Password", show="*")
-        self.password_entry.grid(row=3, column=0, pady=(0, 15), padx=10, sticky="ns")
+        self.password_entry = customtkinter.CTkEntry(master=self.middle_frame, placeholder_text="Password", show="*",
+                                                     width=200)
+        self.password_entry.grid(row=3, column=0, pady=(0, 15), padx=10, sticky="n")
 
         self.remember_me_checkbox = customtkinter.CTkCheckBox(master=self.middle_frame, text="Remember Me")
-        self.remember_me_checkbox.grid(row=4, column=0, pady=(0, 20), padx=10, sticky="ns")
+        self.remember_me_checkbox.grid(row=4, column=0, pady=(0, 20), padx=10, sticky="n")
 
-        self.login_button = customtkinter.CTkButton(master=self.middle_frame, text="Login", command=self.login)
-        self.login_button.grid(row=5, column=0, pady=(0, 10), padx=10, sticky="ns")
+        self.login_button = customtkinter.CTkButton(master=self.middle_frame, text="Login", command=self.login,
+                                                    width=200)
+        self.login_button.grid(row=5, column=0, pady=(0, 10), padx=10, sticky="n")
 
         self.sign_up_button = customtkinter.CTkButton(master=self.middle_frame, text="Sign Up",
-                                                      command=self.from_login_page_to_sign_up_page)
-        self.sign_up_button.grid(row=6, column=0, pady=(0, 10), padx=10, sticky="ns")
+                                                      command=self.from_login_page_to_sign_up_page, width=200)
+        self.sign_up_button.grid(row=6, column=0, pady=(0, 10), padx=10, sticky="n")
 
         # self.temp.grid(row=1, column=1, sticky="e")
         # self.temp2.grid(row=1, column=0, sticky="w")
